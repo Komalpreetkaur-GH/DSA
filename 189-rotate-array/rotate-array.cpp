8a22d6c2babe07407ff514c34e81870be1752ca3
Komalpreet Kaur <150986876+Komalpreetkaur-GH@users.noreply.github.com>
2025-07-03 12:38:17 +0530

    Time: 0 ms (100.00%) | Memory: 29.5 MB (57.52%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -1,31 +1,54 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        int n=nums.size();
-        k=k%n;
-        int left=0;
-        int right=n-1;
-        while(left<right){
-            swap(nums[left],nums[right]);
-            left++;
-            right--;
-        }
-        int i=0;
-        int j=k-1;
-        while(i<j){
-               swap(nums[i],nums[j]);
-            i++;
-            j--;
-
-        }
-        int x=k;
-        int y=n-1;
-        while(x<y){
-               swap(nums[x],nums[y]);
-            x++;
-            y--;
-
-        }
-
+       
+        // so what i came up with was like a 2 pointer thingy wher what i do is , each time i will do is , for n times  a loop will work and what it does is , we have one pointer fixed at end and we have another which starts from 0 index and traverses till second last swapping elements
+        // lemme just quick code it
+        // int s,f;
+        // while(k){
+        // s=0;
+        // f=nums.size()-1;
+        // while(s!=nums.size()-1){
+        //     swap(nums[s],nums[f]);
+        //     s++;
+        // }
+        // k--;     
+        // }
+        // what are you trying to do?
+      
+        // while(k){
+        //     nums.insert(nums.begin(),nums[nums.size()-1]);
+        //     nums.pop_back();
+        //     k--;
+        
+        // }
+        
+    //     reverse(nums.begin(),nums.end());
+   
+    //     k=k%nums.size();
+    //     int i=0;
+    //     int j=k-1;
+    //     while(i<j){
+    //         swap(nums[i],nums[j]);
+            
+    //         i++;
+    //         j--;
+    //     }
+    //     i=k;
+    //     j=nums.size()-1;
+    //     while(i<j){
+    //         swap(nums[i],nums[j]);
+    //         i++;
+    //         j--;
+    //     }
+    // okays....to uska invitation alg se hoga?
+    // oii!!
+    // } 
+    int n=nums.size(); 
+     k=k%n;
+    reverse(nums.begin(),nums.end());
+    reverse(nums.begin(),nums.begin()+k);
+    reverse(nums.begin()+k,nums.end());
+   
     }
 };
