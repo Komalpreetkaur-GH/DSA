fc09e32c478af6510d421818ccd10a9a1886246d
Komalpreet Kaur <150986876+Komalpreetkaur-GH@users.noreply.github.com>
2025-07-24 09:22:31 +0530

    Time: 0 ms (100%), Space: 121 MB (54.65%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -10,25 +10,65 @@
  */
 class Solution {
 public:
+    ListNode* reversell(ListNode* head){
+        ListNode* prev=nullptr;
+        ListNode* temp=head;
+        while(temp){
+            ListNode* front=temp->next;
+            temp->next=prev;
+            prev=temp;
+            temp=front;
+
+        }
+        return prev;
+
+    }
     bool isPalindrome(ListNode* head) {
         if(head==nullptr || head->next==nullptr){
             return true;
         }
-        stack<int> store;
+    //     stack<int> store;
        
-       ListNode* temp=head;
-       while(temp){
-        store.push(temp->val);
-        temp=temp->next;
-       }
-       temp=head;
-       while(temp){
-        if(store.top()!=temp->val){
+    //    ListNode* temp=head;
+    //    while(temp){
+    //     store.push(temp->val);
+    //     temp=temp->next;
+    //    }
+    //    temp=head;
+    //    while(temp){
+    //     if(store.top()!=temp->val){
+    //         return false;
+    //     }
+    //     store.pop();
+    //     temp=temp->next;
+    //    }
+    //     return true;
+
+
+
+    // ************************************
+    ListNode* fast=head;
+    ListNode* slow=head;
+
+    while(fast!=nullptr&& fast->next!=nullptr){
+        fast=fast->next->next;
+        slow=slow->next;
+    }
+    ListNode* newhead=reversell(slow);
+    ListNode* firhead=head;
+    ListNode* sechead=newhead;
+
+    while(sechead){
+        if(sechead->val!=firhead->val){
+            reversell(newhead);
             return false;
+
         }
-        store.pop();
-        temp=temp->next;
-       }
-        return true;
+        sechead=sechead->next;
+        firhead=firhead->next;
+
+    }
+    return true;
+
     }
 };
