d7e9829455bf8449bfabde47816e1a6e74b383f5
Komalpreet Kaur <150986876+Komalpreetkaur-GH@users.noreply.github.com>
2025-08-13 15:55:27 +0530

    Time: 0 ms (100%), Space: 9.8 MB (26.29%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -4,14 +4,20 @@ public:
         if(s.size()!=t.size()){
             return false;
         }
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        if(s==t){
-            return true;
+       vector<int>freq(26,0);
+       for(char c:s){
+        freq[c-'a']++;
+       }
+       for(char c:t){
+        freq[c-'a']--;
+       }
+       
+       for(int i:freq){
+        if(i!=0){
+            return false;
         }
-        return false;
-
-
-        
+       }
+      
+       return true; 
     }
 };
