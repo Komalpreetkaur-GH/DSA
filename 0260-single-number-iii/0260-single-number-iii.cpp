e7c6068765f72f5383bc0731caa18b130f874045
Komalpreet Kaur <150986876+Komalpreetkaur-GH@users.noreply.github.com>
2025-09-04 16:00:07 +0530

    Time: 0 ms (100%), Space: 13.9 MB (82.33%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.cpp
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -2,17 +2,36 @@ class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
         int n=nums.size();
-        map<int,int>freq;
+        // map<int,int>freq;
 
-        for(int i:nums){
-            freq[i]++; 
+        // for(int i:nums){
+        //     freq[i]++; 
+        // }
+        // vector<int> res;
+        // for(auto i: freq){
+        //     if(i.second==1){
+        //         res.push_back(i.first);
+        //     }
+        // }
+        // return res;
+        //METHOD II
+        vector<int>res;
+        int sum=0;
+      for(int n:nums){
+        sum^=n;
+
+      }
+      int diffbit=sum& (int)(-(unsigned int)sum);
+        int a=0;
+        int b=0;
+      for(int i:nums){
+        if(diffbit&i){
+            a^=i;
         }
-        vector<int> res;
-        for(auto i: freq){
-            if(i.second==1){
-                res.push_back(i.first);
-            }
+        else{
+            b^=i;
         }
-        return res;
+      }
+      return {a,b};
     }
 };
