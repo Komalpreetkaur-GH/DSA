d57e0cbd7f6316342e5e222bf4f15311584c931a
Komalpreet Kaur <150986876+Komalpreetkaur-GH@users.noreply.github.com>
2025-09-12 00:51:51 +0530

    Time: 53 ms (5.22%), Space: 16.2 MB (66.53%) - LeetHub

--- 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.cpp
+++ 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.cpp
@@ -1,21 +1,21 @@
+bool isVowel(char ch) {
+    string vowels = "aeiouAEIOU";
+    return vowels.find(ch) != string::npos;
+}
 class Solution {
 public:
     string sortVowels(string s) {
         vector<char> vow;
         int n=s.size();
         for(char ch:s){
-            if(ch=='a' || ch =='e' || ch=='i' || ch=='o' || ch=='u' || ch=='A' || ch=='E' ||
-            ch=='I' || ch=='O' || ch=='U'
-            ){
+            if(isVowel(ch)){
                 vow.push_back(ch);
             }
         }
         int k=0;
         sort(vow.begin(),vow.end());
         for(int i=0;i<n;i++){
-            if(s[i]=='a' || s[i] =='e' || s[i]=='i' || s[i]=='o' || s[i]=='u' || s[i]=='A' || s[i]=='E' ||
-            s[i]=='I' || s[i]=='O' || s[i]=='U'
-            ){
+             if(isVowel(s[i])){
                 s[i]=vow[k++];
 
 
