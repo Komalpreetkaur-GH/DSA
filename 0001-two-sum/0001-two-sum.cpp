2256513f98689b9e6c4444cdd6d5a1a54e0390a6
Komalpreet Kaur <150986876+Komalpreetkaur-GH@users.noreply.github.com>
2025-09-13 10:10:39 +0530

    Time: 0 ms (100%), Space: 14.9 MB (20.26%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int,int> freq;
+
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            int comp=target-nums[i];
+            if(freq.find(comp)!=freq.end()){
+             return {i,freq[comp]};
+            }
+            freq[nums[i]]=i;
+              
+        }
+       return {}; 
+    }
+};
