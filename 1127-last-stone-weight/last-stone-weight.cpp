e155763f4373c57c287c6c715c006888bebc2151
Komalpreet Kaur <150986876+Komalpreetkaur-GH@users.noreply.github.com>
2026-02-06 13:53:56 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (47.09%) - LeetSync

---
+++ 1127-last-stone-weight/last-stone-weight.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+
+        priority_queue<int> pq;
+
+        for(int n: stones){
+            pq.push(n);
+        }
+
+        while(pq.size()>1){
+            int x=pq.top();
+            pq.pop();
+            int y=pq.top();
+            pq.pop();
+
+            if(x!=y){
+                pq.push(x-y);
+            }
+
+
+        }
+
+        return pq.empty()?0:pq.top(); 
+        
+    }
+};
